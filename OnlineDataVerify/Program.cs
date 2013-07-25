ad5bae06c334ea4b70dc25b144a255699cac0457
Laptop <pieterf33@gmail.com>
2013-07-25 10:18:26 +0200

    wip

--- OnlineDataVerify/Program.cs
+++ OnlineDataVerify/Program.cs
@@ -5,34 +5,43 @@ using System.Text;
 
 namespace OnlineDataVerify
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			Console.WriteLine("Checking last few days:\n");
-			AlsiUtils.WebSettings.GetSettings();
-			var data = AlsiTrade_Backend.HiSat.HistData.GetHistoricalMINUTE_FromWEB(DateTime.Now.AddDays(-12), DateTime.Now, 1, AlsiUtils.WebSettings.General.HISAT_INST);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Checking last few days:\n");
+            AlsiUtils.WebSettings.GetSettings();
+            var data = AlsiTrade_Backend.HiSat.HistData.GetHistoricalMINUTE_FromWEB(DateTime.Now.AddDays(-10),
+                DateTime.Now, 1, AlsiUtils.WebSettings.General.HISAT_INST);
 
 
-			
-
-			for (int x = 0; x < 10; x++)
-			{
-				if (DateTime.Now.AddDays(-x).DayOfWeek != DayOfWeek.Saturday || DateTime.Now.AddDays(-x).DayOfWeek != DayOfWeek.Sunday)
-				{
-					var T = data.Where(z => z.TimeStamp.Date == DateTime.Now.Date.AddDays(-x));
-					Console.WriteLine("{0} {1} {2}", DateTime.Now.Date.AddDays(-x).DayOfWeek.ToString().PadRight(10), 
-						DateTime.Now.Date.Date.ToShortDateString().PadRight(10),T.Count()==0?"!!!":T.Count().ToString());
-
-				
-				}
-
-				
-			}
 
 
-			Console.ReadLine();
+            for (int x = 0; x < 10; x++)
+            {
+                if (DateTime.Now.AddDays(-x).DayOfWeek != DayOfWeek.Saturday)
+                    if (DateTime.Now.AddDays(-x).DayOfWeek != DayOfWeek.Sunday)
+                    {
+                        var T = data.Where(z => z.TimeStamp.Date == DateTime.Now.Date.AddDays(-x));
+                        var count=T.Count() == 0;
+                        Console.WriteLine("{0} {1} {2}", DateTime.Now.Date.AddDays(-x).DayOfWeek.ToString().PadRight(10),
+                            DateTime.Now.Date.Date.ToShortDateString().PadRight(10), count ? "!!!" : T.Count().ToString());
+                        if (count)
+                        {
+                            Console.WriteLine("Error Found");
+                            Communicator.Gmail.SendEmail("Pieterf33@gmail.com", "HiSat Data Failed",
+                                "This is an automated email.\nThe alsi future data is not available for "
+                            + DateTime.Now.Date.AddDays(-x).ToShortDateString(),
+                                null, "Pieterf33@gmail.com", "1rachelle", "Alsi Trading System", true);
+                        }
 
-		}
-	}
+                    }
+
+
+            }
+
+
+        
+        }
+    }
 }
